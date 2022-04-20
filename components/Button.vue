b482e0d1147896968bf5f66586249cf28e27da16
designbyfaizi <faizanullah1999@gmail.com>
2022-04-20 16:57:39 +0500

    Testing

---
+++ components/Button.vue
@@ -0,0 +1,17 @@
+<template>
+    <button :style="{backgroundColor: color}"><slot/></button>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+    color:{
+        type: String,
+        required: false,
+        default: '#2fa'
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
